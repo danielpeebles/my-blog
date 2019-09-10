432ff9951a0cf494aa45c018fe800fe042fe0a51
danielpeebles <danielpeebles@hotmail.com>
2019-09-10 14:52:33 -0400

    first commit

---
+++ src/build/precache-manifest.17fcbd4b53eb5b937e24c0011c91b4ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4478b5bdf09fd47c4d0086acaae38f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78071a9472887c3b8137",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "623b6645468bf88ac080",
+    "url": "/static/js/2.99e789c6.chunk.js"
+  },
+  {
+    "revision": "78071a9472887c3b8137",
+    "url": "/static/js/main.ef2c4402.chunk.js"
+  },
+  {
+    "revision": "eeab855a9420d0df9e27",
+    "url": "/static/js/runtime~main.a3235f1f.js"
+  }
+]);
